d5b3125f57b9636d3aeb36742db41af55fc02a90
rchleung <43100790+rchleung@users.noreply.github.com>
2022-05-23 23:51:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
 
   var nine = function(number) {
     if (number == 0) {
-      return 0;
+      return 5;
     }
     
     var remainder = number % 9;
@@ -45,6 +45,22 @@
     u.innerHTML = nine(e.innerHTML+k.innerHTML+h.innerHTML);
     v.innerHTML = nine(q.innerHTML+n.innerHTML+t.innerHTML);
     w.innerHTML = nine(k.innerHTML*2);
+
+    ijk.innerHTML = i.innerHTML+j.innerHTML+k.innerHTML;
+    efi.innerHTML = e.innerHTML+f.innerHTML+i.innerHTML;
+    ghj.innerHTML = g.innerHTML+h.innerHTML+j.innerHTML;
+    opq.innerHTML = o.innerHTML+p.innerHTML+q.innerHTML;
+    lmn.innerHTML = l.innerHTML+m.innerHTML+n.innerHTML;
+    rst.innerHTML = r.innerHTML+s.innerHTML+t.innerHTML;
+
+    eio.innerHTML = e.innerHTML+i.innerHTML+o.innerHTML;
+    fip.innerHTML = f.innerHTML+i.innerHTML+p.innerHTML;
+    gjr.innerHTML = g.innerHTML+j.innerHTML+r.innerHTML;
+    hjs.innerHTML = h.innerHTML+j.innerHTML+s.innerHTML;
+    ikm.innerHTML = i.innerHTML+k.innerHTML+m.innerHTML;
+    jkl.innerHTML = j.innerHTML+k.innerHTML+l.innerHTML;
+
+    hidden.innerHTML = nine(i.innerHTML*2).toString()+nine(j.innerHTML*2).toString()+nine(k.innerHTML*2).toString();
   };
 
   // Add click event to calculate button
